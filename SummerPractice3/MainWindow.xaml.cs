63e62aaedf239aead616381e564463f513c43c12
oleyon <subbotin-2000@mail.ru>
2022-07-15 08:09:17 +0300

    last

--- SummerPractice3/MainWindow.xaml.cs
+++ SummerPractice3/MainWindow.xaml.cs
@@ -1,21 +1,12 @@
 ﻿using SummerPractice.Db.Context;
 using SummerPractice3.Db.Entities;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SummerPractice3
 {
@@ -49,7 +40,6 @@ namespace SummerPractice3
                 {
                     materialSuppliers.Add(supplier);
                 }
-                //label1.Content = suppliers[0].Name;
                 mainGrid.ItemsSource = list;
                 mainGrid.Columns[0].Visibility = Visibility.Collapsed;
             }
